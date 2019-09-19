b062b1b6b1e0c26e2189db83973b631950684a48
fwyaime <fw@abakusitsolutions.eu>
2019-09-19 14:03:46 +0200

    [FIX] Bug when save SN + can edit lot entered if default_name in context

--- stock_serial_default_code_prefix/models/stock_production_lot.py
+++ stock_serial_default_code_prefix/models/stock_production_lot.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
-    name = fields.Char(default="New", compute="_compute_serial_number", store=True)
+    name = fields.Char(default=lambda x: x._compute_serial_number())
 
     @api.multi
     @api.onchange('product_id')
@@ -16,4 +16,7 @@ class StockProductionLot(models.Model):
             if not lot.product_id and not lot.product_id.default_code:
                 raise exceptions.ValidationError(_("A Product must be selected"))
             else:
-                lot['name'] = lot.product_id.default_code.strip('0').zfill(5) + self.env['ir.sequence'].next_by_code('stock.lot.serial')
+                if self.env.context.get('default_name'):
+                    lot.name = self.env.context.get('default_name')
+                else:
+                    lot.name = lot.product_id.default_code.strip('0').zfill(5) + self.env['ir.sequence'].next_by_code('stock.lot.serial')
