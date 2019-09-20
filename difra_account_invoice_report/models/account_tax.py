e13734751683b999865e3420552c4d160ef94b33
Valentin Thirion <valentinthirion@gmail.com>
2019-09-20 16:08:53 +0200

    [CHG] Changed tax table as requested + added currency in the line

--- difra_account_invoice_report/models/account_tax.py
+++ difra_account_invoice_report/models/account_tax.py
@@ -10,6 +10,5 @@ _logger = logging.getLogger(__name__)
 class AccountTax(models.Model):
     _inherit = 'account.tax'
 
-    display_detailled_line = fields.Boolean(string="Display a detailled line for this tax on the invoice report")
+    display_detailled_line = fields.Boolean(string="Display a detailled line for this tax on the invoice report and hide it in the tax table")
     show_base_line = fields.Boolean(string="Display a base line in the tax table before the line")
-
